f69995519bf4bf3f45f04fad36a91f3954abdb78
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-01-27 15:08:16 -0500

    change get_recipes to find_recipes in bin/recipes

--- lib/scraper_logic.rb
+++ lib/scraper_logic.rb
@@ -36,28 +36,3 @@ class Scraper
     end
   end
 end
-
-class Recipes < Scraper
-  def initialize(category)
-    @category = category
-    super
-    titles = @doc.css(".card__title").map { |title| title.content.strip }
-    descriptions = @doc.css(".card__summary").map { |summary| summary.content.strip }
-    recipes_url = @doc.css(".card__detailsContainer-left>.card__titleLink").map { |link| link["href"] }
-    reviews = @doc.css(".card__ratingCount").map { |review| review.content.strip }
-    authors = @doc.css(".card__authorName").map { |author| author.content.strip }
-    recipe_obj(titles, descriptions, recipes_url, reviews, authors)
-  end
-end
-
-class Menu
-  def print_menu
-    <<-HEREDOC
-    #{COLOR_2}1.#{COLOR_END}  Breakfast and Brunch | #{COLOR_2}2. #{COLOR_END} Lunch
-    #{COLOR_2}3.#{COLOR_END}  Dinners              | #{COLOR_2}4. #{COLOR_END} Appetizer and Snacks
-    #{COLOR_2}5.#{COLOR_END}  Breads               | #{COLOR_2}6. #{COLOR_END} Desserts
-    #{COLOR_2}7.#{COLOR_END}  Drinks               | #{COLOR_2}8. #{COLOR_END} Salads
-    #{COLOR_2}9.#{COLOR_END}  Side Dishes          | #{COLOR_2}10.#{COLOR_END} Soups and Stews
-    HEREDOC
-  end
-end
