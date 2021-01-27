f87acb44d690895262a419315d4ec90078315f3b
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-01-27 15:22:43 -0500

    Change menu layout

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,13 @@
-require_relative './styles'
+require_relative "./styles"
 
 class Menu
   def print_menu
-    options = ['Breakfast and Brunch', 'Lunch', 'Dinners', 'Appetizer and Snacks', 'Breads', 'Desserts', 'Drinks',
-               'Salads', 'Side Dishes', 'Soups and Stews']
-    options.each_with_index do |option, index|
-      puts "#{COLOR_2} #{index + 1}.#{COLOR_END} #{option}"
-    end
+    <<-HEREDOC
+    #{COLOR_2}1.#{COLOR_END}  Breakfast and Brunch | #{COLOR_2}2. #{COLOR_END} Lunch
+    #{COLOR_2}3.#{COLOR_END}  Dinners              | #{COLOR_2}4. #{COLOR_END} Appetizer and Snacks
+    #{COLOR_2}5.#{COLOR_END}  Breads               | #{COLOR_2}6. #{COLOR_END} Desserts
+    #{COLOR_2}7.#{COLOR_END}  Drinks               | #{COLOR_2}8. #{COLOR_END} Salads
+    #{COLOR_2}9.#{COLOR_END}  Side Dishes          | #{COLOR_2}10.#{COLOR_END} Soups and Stews
+    HEREDOC
   end
 end
