1811410533e6e6ff317ef1af53689336a71868c5
Jaim A.D.D <69362254+jadx2@users.noreply.github.com>
2021-01-27 17:22:53 -0500

    Update recipes_scraper_spec.rb

--- spec/recipes_scraper_spec.rb
+++ spec/recipes_scraper_spec.rb
@@ -1,23 +1,23 @@
 # spec/recipes_scraper_spec.rb
-require_relative "../lib/menu.rb"
-require_relative "../lib/recipes.rb"
+require_relative '../lib/menu.rb'
+require_relative '../lib/recipes.rb'
 
 describe Menu do
-  describe "#validator" do
-    it "verifies if the input is a number returns true" do
+  describe '#validator' do
+    it 'verifies if the input is a number returns true' do
       selection = 3
       expect(selection.is_a?(Numeric)).to eql true
     end
-    it "verifies if the input is not a number returns false" do
-      selection = "d"
+    it 'verifies if the input is not a number returns false' do
+      selection = 'd'
       expect(selection.is_a?(Numeric)).to eql false
     end
 
-    it "verifies if the input is between 1 and 10 returns true" do
+    it 'verifies if the input is between 1 and 10 returns true' do
       selection = 5
       expect(selection.between?(1, 10)).to eql true
     end
-    it "verifies if the input is not between 1 and 10 returns false" do
+    it 'verifies if the input is not between 1 and 10 returns false' do
       selection = 11
       expect(selection.between?(1, 10)).to eql false
     end
@@ -25,8 +25,8 @@ describe Menu do
 end
 
 describe Recipes do
-  describe "#initialize" do
-    it "creates a new instance for the recipes class" do
+  describe '#initialize' do
+    it 'creates a new instance for the recipes class' do
       recipes = Recipes.new(1)
       expect(recipes).to be_a(Recipes)
     end
