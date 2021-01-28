93488144ed6581fbfbf7f978299f1e9093eb2e65
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-01-28 09:00:48 -0500

    Move print recipe to recipes file

--- lib/scraper_logic.rb
+++ lib/scraper_logic.rb
@@ -1,6 +1,6 @@
-require 'httparty'
-require 'nokogiri'
-require_relative './styles'
+require "httparty"
+require "nokogiri"
+require_relative "./styles"
 
 class Scraper
   def initialize(_category)
@@ -8,16 +8,8 @@ class Scraper
     @doc = Nokogiri::HTML(html.body)
   end
 
-  def print_recipe
-    @recipes.each do |recipe|
-      puts "#{COLOR_1}Recipe #{recipe[:index] + 1}: #{recipe[:title]}#{COLOR_END}"
-      puts "\n"
-      puts "#{COLOR_2}Summary:#{COLOR_END} #{recipe[:description]}"
-      puts "#{COLOR_2}Reviews:#{COLOR_END} #{recipe[:review]}"
-      puts "#{COLOR_2}Recipe URL:#{COLOR_END} #{recipe[:recipes_url]}"
-      puts "#{COLOR_2}Author:#{COLOR_END} #{recipe[:author]}"
-      puts '-------------------------------------------------------'
-    end
+  def recipes_holder
+    @recipes
   end
 
   private
@@ -31,7 +23,7 @@ class Scraper
         description: descriptions[i],
         recipes_url: recipes_url[i],
         review: reviews[i],
-        author: authors[i]
+        author: authors[i],
       }
     end
   end
